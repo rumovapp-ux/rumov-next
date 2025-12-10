4d9093f423f728dcf188cfb7d58324e6af9255ee
Soso <rumovapp@gmail.com>
2025-12-10 18:00:54 +0100

    retest

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -5,8 +5,8 @@ import { createClient } from "@supabase/supabase-js";
 export default function TestPage() {
 
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!
   );
 
   async function testSupabase() {
